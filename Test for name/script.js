9365137ca92a629f632a740cd3b4f3f52653ac47
Da-bee-122 <seyhatep707@gmail.com>
2025-06-05 23:38:49 +0700

    Add files via upload

---
+++ Test for name/script.js
@@ -0,0 +1,8 @@
+// Example dynamic behavior
+document.addEventListener("DOMContentLoaded", () => {
+  const groupName = document.getElementById("group-name");
+  groupName.style.color = "#00ffff";
+
+  const teacherName = document.getElementById("teacher-name");
+  teacherName.style.fontWeight = "bold";
+});
